a8a5dd8e9b6af7ba5bcca10804ab271600109109
daudassidiqi <assidiqidaud@gmail.com>
2023-12-24 01:23:09 +0700

    numbers & math

---
+++ Numbers & Operasi Matematika/numbers.py
@@ -0,0 +1,13 @@
+a = 5
+b = 3
+c = 4.0
+
+# Penjumlahan
+
+print("penjumlahan a + b =" ,a + b )
+
+# Pengurangan
+print("penjumlahan a - b =" ,a - b )
+
+# Perkalian
+print("Perkalian a * c = ", a * c)
