7699f62e279d0323f313ad2a85b49f7e3db7701f
daudassidiqi <assidiqidaud@gmail.com>
2023-12-24 18:14:58 +0700

    strings

---
+++ strings/string2.py
@@ -0,0 +1,4 @@
+text = "uang seratus"
+a = text[5:]
+
+print('pinjam'+a)
---
+++ strings/strings.py
@@ -0,0 +1,15 @@
+text1 = 'ini adalah string'
+text2 = 'contoh kalo mau pake\'kutip '
+text3 = "contoh lagi kalo mau pake ku'tip"
+text4 = 'yang nanya : "kalo kalimat beda paragraph gimana bre?"'
+text5 = 'yang jawab : "kek gini aja" \n biar "gitu"'
+text6 = r'C:\folder' #raw string
+
+
+print(text1)
+print(text2)
+print(text3)
+print(text4)
+print(text5)
+print(text6)
+print(100*"punyaku")
